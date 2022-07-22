1fa51d74aeaa1cc97a37aac796b58cb2e8b53872
SdtElectronics <meiyang423@Gmail.com>
2022-07-22 22:33:52 +0800

    Add interoperability tests
    Add tests verifying the interoperability between Zlib and muzic

---
+++ tests/interop.sh
@@ -0,0 +1,41 @@
+# 
+
+assertIdentical(){
+    if [[ $1 != $2 ]]; then
+        printf "$1 \n and \n $2 mismatch \n"
+        exit -1
+    fi
+}
+
+sample="Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+ sed do eiusmod tempor incididunt ut labore et dolore magna     \
+ aliqua. Ut enim ad minim veniam, quis nostrud exercitation     \
+ ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis  \
+ aute irure dolor in reprehenderit in voluptate velit esse      \
+ cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat\
+ cupidatat non proident, sunt in culpa qui officia deserunt     \
+ mollit anim id est laborum."
+
+deflBins=(muzicDefl muzicDeflFragIn muzicDeflFragOut zlibDefl zlibDeflFragIn)
+inflBins=(muzicInfl zlibInfl zlibInflFragIn)
+
+for defl in ${deflBins[@]}; do
+    for infl in ${inflBins[@]}; do
+        if [[ ($defl == "zlib"* && $infl == "zlib"*) || ($defl == "muzic"* && $infl == "muzic"*) ]]
+        then
+            continue
+        fi
+
+        echo "Test case $defl to $infl"
+        cmd="echo \"${sample}\" | ${TEST_BINARY_PATH}/${defl} 1 1 | ${TEST_BINARY_PATH}/${infl}"
+        echo "$cmd"
+        result=$(eval "$cmd")
+        ret=$?
+        if [[ $ret != 0 ]]; then
+            echo "FAILED with $ret"
+            exit $ret
+        fi
+        assertIdentical "$sample" "$result"
+        printf "PASSED\n\n"
+    done
+done
